c3f3c9eafd791afca5e8a393dec80188435ad354
Federico Baldassarre <baldassarre.fe@gmail.com>
2020-02-16 18:33:49 +0100

    Visual relations recall at K for predicate/phrase/relationship detection

--- src/xib/ignite/metrics.py
+++ src/xib/ignite/metrics.py
@@ -10,11 +10,14 @@ import numpy as np
 import sklearn.metrics
 from collections import defaultdict
 
+from tensorboardX import SummaryWriter
 from ignite.engine import Events, Engine
 from ignite.metrics import Metric
-from tensorboardX import SummaryWriter
+from torchvision.ops import box_iou
+from torch_geometric.data import Batch
+from torch_geometric.utils import scatter_
 
-from ..structures import ImageSize, Vocabulary
+from ..structures import ImageSize, Vocabulary, matched_boxlist_union
 
 
 class BatchMetric(Metric, ABC):
@@ -531,65 +534,180 @@ class VisualRelationPredictionLogger(object):
 
 
 class VisualRelationRecallAt(object):
-
     class Mode(Enum):
-        PREDICATE_PREDICTION = 0
-        PHRASE_DETECTION = 1
-        RELATIONSHIP_DETECTION = 2
+        PREDICATE_DETECTION = 'predicate'
+        PHRASE_DETECTION = 'phrase'
+        RELATIONSHIP_DETECTION = 'relation'
+
+        def __init__(self, short_name):
+            self.short_name = short_name
 
     def __init__(self, mode, steps: Tuple[int, ...]):
+        if isinstance(mode, str):
+            mode = VisualRelationRecallAt.Mode[mode]
+        if not isinstance(mode, VisualRelationRecallAt.Mode):
+            raise ValueError(f'Invalid visual relations mode: {mode}')
+
         self.mode = mode
-        self.steps = sorted(steps)
+        self.steps = torch.tensor(sorted(steps))
 
     def __call__(self, engine: Engine):
-        """Compute Recall @ self.steps for the current batch"""
-        relations = engine.state.output['relations']
+        predictions = engine.state.output['relations']
         targets = engine.state.batch[1]
 
-        # vr_tuple = (subj_idx, subj_class, predicate_class, obj_idx, obj_class)
-        predicted_relations = defaultdict(list)
-        for graph_idx, vr_tuple in zip(
-                relations.batch[relations.relation_indexes[0]].numpy(), zip(
-                    relations.relation_indexes[0].numpy(),
-                    relations.object_classes[relations.relation_indexes[0]].numpy(),
-                    relations.predicate_classes.numpy(),
-                    relations.relation_indexes[1].numpy(),
-                    relations.object_classes[relations.relation_indexes[1]].numpy(),
-                )
-        ):
-            if len(predicted_relations[graph_idx]) >= self.steps[-1]:
-                # Keep at most max(self.steps) relations per graph
-                continue
-            predicted_relations[graph_idx].append(vr_tuple)
+        if self.mode is VisualRelationRecallAt.Mode.PREDICATE_DETECTION:
+            matches = self._predicate_detection(predictions, targets)
+        elif self.mode is VisualRelationRecallAt.Mode.PHRASE_DETECTION:
+            matches = self._phrase_detection(predictions, targets)
+        elif self.mode is VisualRelationRecallAt.Mode.RELATIONSHIP_DETECTION:
+            matches = self._relationship_detection(predictions, targets)
+        else:
+            raise ValueError(f'Invalid visual relations mode: {self.mode}')
 
-        relevant_items_retrieved_at = {k: torch.zeros_like(targets.n_edges) for k in self.steps}
-        relevant_items = targets.n_edges
+        recall_at = self._recall_at(predictions, targets, matches)
+        for k, r in recall_at.items():
+            engine.state.output[f'{self.mode.short_name}/recall_at_{k}'] = r
 
-        for graph_idx, vr_tuple in zip(
-                targets.batch[targets.relation_indexes[0]].numpy(), zip(
-                    targets.relation_indexes[0].numpy(),
-                    targets.object_classes[targets.relation_indexes[0]].numpy(),
-                    targets.predicate_classes.numpy(),
-                    targets.relation_indexes[1].numpy(),
-                    targets.object_classes[targets.relation_indexes[1]].numpy(),
-                )
-        ):
-            try:
-                found_at = predicted_relations[graph_idx].index(vr_tuple)
-            except ValueError:
-                # Ground-truth relation not found in the retrieved relations, keep going
-                continue
+    @staticmethod
+    def _predicate_detection(predictions: Batch, targets: Batch) -> torch.Tensor:
+        """Computes matches based on "predicate detection" mode."""
+        # [E_t, 5]
+        gt_matrix = torch.stack([
+            # subject_idx, object_idx
+            targets.batch[targets.relation_indexes[0]],
+            targets.batch[targets.relation_indexes[1]],
+            # subject_class, predicate_class, object_class
+            targets.object_classes[targets.relation_indexes[0]],
+            targets.predicate_classes,
+            targets.object_classes[targets.relation_indexes[1]],
+        ], dim=1)
 
-            for k, relevant_items_retrieved in relevant_items_retrieved_at.items():
-                if found_at >= k:
-                    # the relevant_items_retrieved_at dict is ordered, so we can break early
-                    break
-                relevant_items_retrieved[graph_idx] += 1
+        # [E_p, 5]
+        pred_matrix = torch.stack([
+            # subject_idx, object_idx
+            predictions.batch[predictions.relation_indexes[0]],
+            predictions.batch[predictions.relation_indexes[1]],
+            # subject_class, predicate_class, object_class
+            predictions.object_classes[predictions.relation_indexes[0]],
+            predictions.predicate_classes,
+            predictions.object_classes[predictions.relation_indexes[1]],
+        ], dim=1)
 
-        recall_per_graph_at = {
-            k: relevant_items_retrieved.float() / relevant_items
-            for k, relevant_items_retrieved in relevant_items_retrieved_at.items()
-        }
+        # Block matrix [E_p, E_t]
+        matches = (gt_matrix[None, :, :] == pred_matrix[:, None, :]).all(dim=2)
 
-        for k, recall_per_graph in recall_per_graph_at.items():
-            engine.state.output[f'recall_at_{k}'] = recall_per_graph.mean().item()
+        return matches
+
+    @staticmethod
+    def _phrase_detection(predictions: Batch, targets: Batch) -> torch.Tensor:
+        """Computes matches based on "phrase detection" mode."""
+        # [E_p, 4]
+        pred_matrix = torch.stack([
+            # graph_idx
+            predictions.batch[predictions.relation_indexes[0]],
+            # subject_class, predicate_class, object_class
+            predictions.object_classes[predictions.relation_indexes[0]],
+            predictions.predicate_classes,
+            predictions.object_classes[predictions.relation_indexes[1]],
+        ], dim=1)
+
+        # [E_t, 4]
+        gt_matrix = torch.stack([
+            # graph_idx
+            targets.batch[targets.relation_indexes[0]],
+            # subject_class, predicate_class, object_class
+            targets.object_classes[targets.relation_indexes[0]],
+            targets.predicate_classes,
+            targets.object_classes[targets.relation_indexes[1]],
+        ], dim=1)
+
+        # Block matrix [E_p, E_t]
+        matches_class = (gt_matrix[None, :, :] == pred_matrix[:, None, :]).all(dim=2)
+
+        # [E_p, 4]
+        pred_union_boxes = matched_boxlist_union(
+            predictions.object_boxes[predictions.relation_indexes[0]],
+            predictions.object_boxes[predictions.relation_indexes[1]],
+        )
+
+        # [E_t, 4]
+        gt_union_boxes = matched_boxlist_union(
+            targets.object_boxes[targets.relation_indexes[0]],
+            targets.object_boxes[targets.relation_indexes[1]],
+        )
+
+        # Full matrix [E_p, E_t]
+        iou_union = box_iou(pred_union_boxes, gt_union_boxes)
+
+        # Block matrix [E_p, E_t]
+        matches = matches_class & (iou_union > .5)
+
+        return matches
+
+    @staticmethod
+    def _relationship_detection(predictions: Batch, targets: Batch) -> torch.Tensor:
+        """Computes matches based on "relationship detection" mode"""
+        # [E_p, 4]
+        pred_matrix = torch.stack([
+            # graph_idx
+            predictions.batch[predictions.relation_indexes[0]],
+            # subject_class, predicate_class, object_class
+            predictions.object_classes[predictions.relation_indexes[0]],
+            predictions.predicate_classes,
+            predictions.object_classes[predictions.relation_indexes[1]],
+        ], dim=1)
+
+        # [E_t, 4]
+        gt_matrix = torch.stack([
+            # graph_idx
+            targets.batch[targets.relation_indexes[0]],
+            # subject_class, predicate_class, object_class
+            targets.object_classes[targets.relation_indexes[0]],
+            targets.predicate_classes,
+            targets.object_classes[targets.relation_indexes[1]],
+        ], dim=1)
+
+        # Block matrix [E_p, E_t]
+        matches_class = (gt_matrix[None, :, :] == pred_matrix[:, None, :]).all(dim=2)
+
+        # Two full matrices [E_p, E_t]
+        iou_subject = box_iou(
+            predictions.object_boxes[predictions.relation_indexes[0]],
+            targets.object_boxes[targets.relation_indexes[0]]
+        )
+        iou_object = box_iou(
+            predictions.object_boxes[predictions.relation_indexes[1]],
+            targets.object_boxes[targets.relation_indexes[1]]
+        )
+
+        # Block matrix [E_p, E_t]
+        matches = matches_class & (iou_subject > .5) & (iou_object > .5)
+
+        return matches
+
+    def _recall_at(self, predictions: Batch, targets: Batch, matches: torch.Tensor) -> Dict[int, float]:
+        # matches.argmax(dim=0) will return the last index if no True value is found.
+        # We can use matches.any(dim=0) to ignore those cases.
+        # Also, we must account for the row offset in the matches matrix.
+        gt_retrieved = matches.any(dim=0)
+
+        offset = predictions.n_edges.cumsum(dim=0).repeat_interleave(targets.n_edges) - predictions.n_edges[0]
+        gt_retrieved_rank = matches.int().argmax(dim=0) - offset
+
+        # [K, E_t]
+        gt_retrieved_at = (gt_retrieved_rank[None, :] < self.steps[:, None]) & gt_retrieved[None, :]
+
+        # [K, num_graphs]
+        gt_relation_to_graph_assignment = targets.batch[targets.relation_indexes[0]]
+        recall_at_per_graph = scatter_(
+            'mean',
+            gt_retrieved_at.float(),
+            index=gt_relation_to_graph_assignment,
+            dim=1,
+            dim_size=targets.num_graphs
+        )
+
+        # [K]
+        recall_at = recall_at_per_graph.mean(dim=1)
+
+        return {k: v for k, v in zip(self.steps.numpy(), recall_at.numpy())}
